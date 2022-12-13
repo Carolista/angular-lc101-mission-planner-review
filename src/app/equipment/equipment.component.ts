964ee2844508180cebf3a9d9051d67d6e4d85bc4
Carolista <cjones.renaissancewoman@gmail.com>
2022-12-12 20:49:21 -0600

    coded solution during studio review

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = [ 'Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks'];
+
+  itemBeingEdited: string = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+
+  save(newItem: string, item: string) {
+    if (!this.equipment.includes(newItem)) {
+      let index = this.equipment.indexOf(item);
+      this.equipment.splice(index, 1, newItem);
+    }
+    this.itemBeingEdited = null;
+  }
+
+  remove(item: string) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+
+  add(item: string) {
+    if (!this.equipment.includes(item)) {
+      this.equipment.push(item);
+    }
   }
 
 }
